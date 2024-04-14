61a01e52a4823d2ae849eb4078c30c159f420d69
Deatherngai <73379572+Deatherngai@users.noreply.github.com>
2024-04-14 21:25:40 +0800

    Update Report_Temp.php

--- Temp/Report_Temp.php
+++ Temp/Report_Temp.php
@@ -1,6 +1,4 @@
-<link href="../static/css/animation.css" rel="stylesheet" />
-<body translate="no" >
-  <div class="loader"></div>
+
 <?php
 session_start();
 if($_SERVER['REQUEST_METHOD']=="GET"){
@@ -12,4 +10,3 @@ if($_SERVER['REQUEST_METHOD']=="GET"){
   header($url);
 }
 ?>
-</body>
