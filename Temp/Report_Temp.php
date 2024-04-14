4a0e4eb4bf67fd6e3eda9417c112970d163d9de2
Deatherngai <73379572+Deatherngai@users.noreply.github.com>
2024-04-14 20:51:09 +0800

    Update Report_Temp.php

--- Temp/Report_Temp.php
+++ Temp/Report_Temp.php
@@ -8,7 +8,6 @@ if($_SERVER['REQUEST_METHOD']=="GET"){
   if($action == 'report'){
       $_SESSION['report'] = $_GET['reports'];
       $url = 'Location:../public/BookReport.php';
-      echo %url;
   }
   header($url);
 }
