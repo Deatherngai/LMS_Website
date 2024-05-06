8008c61da2a0f5e04bdb730f0853a0033e190feb
Deatherngai <73379572+Deatherngai@users.noreply.github.com>
2024-05-06 15:58:27 +0800

    Update Member_Temp.php

--- Temp/Member_Temp.php
+++ Temp/Member_Temp.php
@@ -6,7 +6,7 @@ if($_SERVER['REQUEST_METHOD']=="GET"){
     $action = $_GET['action'];
     $_SESSION['Member_info'] = $_GET['info'];
     $url = "Location: ../public/AccInfo.php";
-    header($url);
-    exit;
 }
+header($url);
+exit;
 ?>
