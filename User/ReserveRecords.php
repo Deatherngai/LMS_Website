c4bfc90f4c0208864fd618572b2d69eb87b7f390
Deatherngai <73379572+Deatherngai@users.noreply.github.com>
2024-05-11 11:40:27 +0800

    Add files via upload

--- User/ReserveRecords.php
+++ User/ReserveRecords.php
@@ -1,11 +1,25 @@
 <?php
-include('includes/header.php');
-$info = "";
-$n = "";
-if ($_SERVER['REQUEST_METHOD'] == "GET") {
-        $info =  $_GET['query'];
-}
+session_start();
+$libs = json_decode($_SESSION['library_all'],true);
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS --> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link href="../static/css/navbar_m.css" rel="stylesheet">
+    <link href="../static/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../static/css/layout.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <title>Library Management System</title>
+</head>
+
+<body>
 <style>
 table,
 th,
@@ -26,6 +40,29 @@ tr:nth-child(even) {
     background: #FFF;
 }
 </style>
+<div>
+        <?php
+        if (isset($_SESSION['login']) && $_SESSION['login'] == "true") {
+            switch($_SESSION['AccType']){
+                case 'staff':
+                    include('navbar_s.php');
+                    break;
+                case 'leader':
+                    include('navbar_s.php');
+                    break;
+                case 'admin':
+                    include('navbar_a.php');
+                    break;
+                default:
+                    include('navbar.php');
+                    break;
+            }
+        } else {
+            include('navbar.php');
+        }
+        ?>
+    </div>
+<div>
 <h1 id="t"></h1>
 <p id=" info" style="display:none;"><?= $Info; ?></p>
 <div class="container">
@@ -37,7 +74,33 @@ tr:nth-child(even) {
                 </div>
             </div>
             <div id="LB_info" class="card-body">
-                
+            <?php
+                for($i=0;$i<count($libs);$i++){
+                        $stock = json_decode($_SESSION[$libs[$i]['LID'].'_resever'],true);
+                        $books = $libs[$i]['LID'].'_books';
+                        $books = json_decode($_SESSION[$books],true);
+                        $table = '<table><tr><td colspan="7">'.$libs[$i]['Library'].'</td></tr>';
+                        $table .= '<tr><td>Reserver ID</td><td>Book Name</td><td>Author</td></td><td>Maker of appointment</td><td>ApplyDate</td>';
+                        $table .='<td>FixedDate</td><td>Status</td></tr>';
+                        for($x=0;$x<count($books);$x++){
+                                $table .= '<tr><td>'.$stock[$x]['RD'].'</td>';
+                                $table .= '<td>'.$books[$x]['BookName_EN'].'</td>';
+                                $table .='<td>'.$books[$x]['Author'].'</td>';
+                                $table .='<td>'.$stock[$x]['AccID'].'</td>';
+                                $table .='<td>'.date("Y-m-d",$stock[$x]['ApplyDate']['seconds']).'</td>';
+                                if($stock[$x]['Status'] == 'Wait'){
+                                    $table .='<td></td>';
+                                    $table .='<td>'.$stock[$x]['Status'].'</td>';
+                                }else{
+                                    $table .='<td></td>';
+                                    $table .='<td>'.$stock[$x]['Status'].'</td>';
+                                }
+                        }
+                        $table.='</table>';
+                        echo $table;
+						echo "<br />";
+                    }
+                    ?>
             </div>
         </div>
     </div>
